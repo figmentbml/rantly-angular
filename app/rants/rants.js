8ee0493a5c9e56b286213016e18a5c3fd9a202bd
figmentbml <beth.jaswa@gmail.com>
2015-03-05 16:02:20 -0700

    new rant!

--- app/rants/rants.js
+++ app/rants/rants.js
@@ -3,9 +3,14 @@
 var rants = angular.module('rantly.rants', ['ngRoute']);
 
 rants.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/rants', {
+  $routeProvider.
+  when('/rants', {
     templateUrl: 'rants/rants.html',
     controller: 'RantsCtrl'
+  }).
+  when('/rants/new', {
+    templateUrl: 'rants/new.html',
+    controller: 'NewRantCtrl'
   });
 }]);
 
@@ -13,4 +18,13 @@ rants.controller('RantsCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('http://localhost:3000/rants').success(function(data) {
     $scope.rants = data.rants;
   });
+}]);
+
+rants.controller('NewRantCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
+    $scope.createRant = function() {
+      $http.post('http://localhost:3000/rants/', { title: title, body: body });
+      $scope.title = '';
+      $scope.body = '';
+      $location.path('/rants');
+    };
 }]);
