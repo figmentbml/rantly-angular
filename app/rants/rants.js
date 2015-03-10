c97225d73d7a65aed093e1dd9425fd119be1d5a8
figmentbml <beth.jaswa@gmail.com>
2015-03-10 11:26:30 -0600

    new rant works

--- app/rants/rants.js
+++ app/rants/rants.js
@@ -22,9 +22,9 @@ rants.controller('RantsCtrl', ['$scope', '$http', function($scope, $http) {
 
 rants.controller('NewRantCtrl', ['$scope', '$http', '$location', function($scope, $http, $location) {
     $scope.createRant = function() {
-      $http.post('http://localhost:3000/rants/', { title: title, body: body });
+      $http.post('http://localhost:3000/rants/', { title: $scope.title, body: $scope.body });
       $scope.title = '';
       $scope.body = '';
-      $location.path('/rants');
+      $location.path('/');
     };
 }]);
